048d038a89686b28594642992b4af5722c037dbf
Dieter Mourisse <dieter.mourisse@howest.be>
2020-04-20 17:12:53 +0200

    Add project files.

---
+++ Streams/FileCounter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streams
+{
+    class FileCounter
+    {
+
+        public int CountLines(string filename)
+        {
+            return 0;
+        }
+
+        public int Count(string filename, char character)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Streams/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileCounter fc = new FileCounter();
+
+            Console.WriteLine("Aantal meisjesnamen: " + fc.CountLines("meisjes_namen.txt"));
+            Console.WriteLine("Aantal jongensnamen: " + fc.CountLines("jongens_namen.txt"));
+
+            Console.WriteLine("Aantal keer dat de letter 'a' voorkomt in meisjesnamen: " + fc.Count("meisjes_namen.txt", 'a'));
+            Console.WriteLine("Aantal keer dat de letter 'x' voorkomt in jongensnamen: " + fc.Count("jongens_namen.txt", 'x'));
+
+            Console.ReadLine();
+        }
+    }
+}
